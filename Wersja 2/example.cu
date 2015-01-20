892de9f0ad5e4f7f9a8265e4394d53150f7c4e74
gracz21 <kamil.walek21@gmail.com>
2015-01-20 14:18:18 +0100

    Zmiana typu danych i rozmiaru bloku

--- Wersja 2/example.cu
+++ Wersja 2/example.cu
@@ -5,9 +5,9 @@
 #include "device_launch_parameters.h"
 
 
-__global__ void reduce0(int *g_idata, int *g_odata, int size){
+__global__ void reduce0(float *g_idata, float *g_odata, int size){
 
-   extern __shared__ int sdata[];
+   extern __shared__ float sdata[];
 
    unsigned int tid = threadIdx.x;
    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
@@ -27,15 +27,15 @@ __global__ void reduce0(int *g_idata, int *g_odata, int size){
    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
-cudaError_t reduceWithCuda(int *input, int size) {
-	int output = 0;
+cudaError_t reduceWithCuda(float *input, int size) {
+	float output = 0.0;
 	//Liczba w¹tków na blok
-	int threadsPerBlock = 1024;
+	int threadsPerBlock = 256;
 	//Liczba bloków (na pocz¹tku)
 	int totalBlocks = (size+(threadsPerBlock-1))/threadsPerBlock;
 
 	//Wektor wejœciowy i wyjœciowy device
-	int *dev_i, *dev_o;
+	float *dev_i, *dev_o;
 	cudaError_t cudaStatus;
 	
 	cudaStatus = cudaSetDevice(0);
@@ -45,20 +45,20 @@ cudaError_t reduceWithCuda(int *input, int size) {
     }
 	
 	// Allocate GPU buffers for three vectors (two input, one output).
-    cudaStatus = cudaMalloc((void**)&dev_i, size * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_i, size * sizeof(float));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
-    cudaStatus = cudaMalloc((void**)&dev_o, totalBlocks * sizeof(int));
+    cudaStatus = cudaMalloc((void**)&dev_o, totalBlocks * sizeof(float));
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMalloc failed!");
         goto Error;
     }
 
     // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_i, input, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaStatus = cudaMemcpy(dev_i, input, size * sizeof(float), cudaMemcpyHostToDevice);
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaMemcpy failed!");
         goto Error;
@@ -93,10 +93,10 @@ cudaError_t reduceWithCuda(int *input, int size) {
 	  
 	while(true) {	
 		if(turn) {  
-		  reduce0<<<totalBlocks, threadsPerBlock, threadsPerBlock*sizeof(int)>>>(dev_i, dev_o, size);
+		  reduce0<<<totalBlocks, threadsPerBlock, threadsPerBlock*sizeof(float)>>>(dev_i, dev_o, size);
 		  turn = false;
 		} else {
-		  reduce0<<<totalBlocks, threadsPerBlock, threadsPerBlock*sizeof(int)>>>(dev_o, dev_i, size);
+		  reduce0<<<totalBlocks, threadsPerBlock, threadsPerBlock*sizeof(float)>>>(dev_o, dev_i, size);
 		  turn = true;
 		}
 		
@@ -141,20 +141,20 @@ cudaError_t reduceWithCuda(int *input, int size) {
     }
 	  
 	if(turn) {
-		cudaStatus = cudaMemcpy(&output, &dev_i[0], sizeof(int), cudaMemcpyDeviceToHost);
+		cudaStatus = cudaMemcpy(&output, &dev_i[0], sizeof(float), cudaMemcpyDeviceToHost);
 		if (cudaStatus != cudaSuccess) {
 			fprintf(stderr, "cudaMemcpy failed!");
 			goto Error;
 		}
 	} else {
-		cudaStatus = cudaMemcpy(&output, &dev_o[0], sizeof(int), cudaMemcpyDeviceToHost);
+		cudaStatus = cudaMemcpy(&output, &dev_o[0], sizeof(float), cudaMemcpyDeviceToHost);
 		if (cudaStatus != cudaSuccess) {
 			fprintf(stderr, "cudaMemcpy failed!");
 			goto Error;
 		}
 	}
 	
-	printf("Wynik to: %d, w czasie: %f\n", output, msecTotal);
+	printf("Wynik to: %f, w czasie: %f\n", output, msecTotal);
 	
 Error:
     cudaFree(dev_i);
@@ -167,9 +167,9 @@ int main(void) {
 	//Deklaracja rozmiaru
 	int size = 939289;
 	//Wektor wejœciowy hosta
-	int *input = (int*)malloc(size * sizeof(int));
+	float *input = (float*)malloc(size * sizeof(float));
 	for(int i = 0; i < size; i++)
-		input[i] = 1;
+		input[i] = 1.0;
 
 	cudaError_t cudaStatus = reduceWithCuda(input, size);
 	if (cudaStatus != cudaSuccess) {
